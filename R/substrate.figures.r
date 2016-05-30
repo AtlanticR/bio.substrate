c83c398dff608cd6cef70f28376d936994d83038
Jae S Choi <choijaeseok@gmail.com>
2016-05-30 15:05:54 -0300

    deps

--- R/substrate.figures.r
+++ R/substrate.figures.r
@@ -1,13 +1,14 @@
 
 substrate.figures = function( DS=NULL, p=NULL ) {
 
+  ecomodLibrary( "ecomod_coastline" )
+
   if ( DS=="predictions" ) {
-    loadfunctions( "coastline" )
     p = spacetime.db( p=p, DS="bigmemory.filenames" )
     P = attach.big.matrix(p$descriptorfile.P , path=p$tmp.datadir )
     pps  =  expand.grid( plons=p$plons, plats=p$plats)
     p$spatial.domain="canada.east"  # force isobaths to work in levelplot
-    datarange = range( P[ which(is.finite(P[,2])),2] ) 
+    datarange = range( P[ which(is.finite(P[,2])),2] )
     dr = seq( datarange[1], datarange[2], length.out=100)
     oc = landmask( db="worldHires", regions=c("Canada", "US"), return.value="not.land", tag="predictions" )
     levelplot( log( P[oc,2] ) ~ plons + plats, pps[oc,], aspect="iso", main=NULL, at=dr, col.regions=rev(color.code( "seis", dr)) ,
@@ -19,9 +20,8 @@ substrate.figures = function( DS=NULL, p=NULL ) {
       }
     )
   }
-  
+
   if ( DS=="predictions.error" ) {
-    loadfunctions( "coastline" )
     p = spacetime.db( p=p, DS="bigmemory.filenames" )
     P = attach.big.matrix(p$descriptorfile.P , path=p$tmp.datadir )
     pps  =  expand.grid( plons=p$plons, plats=p$plats)
@@ -40,10 +40,9 @@ substrate.figures = function( DS=NULL, p=NULL ) {
   }
 
   if ( DS=="statistics" ) {
-    loadfunctions( "coastline" )
     p = spacetime.db( p=p, DS="bigmemory.filenames" )
-    S = attach.big.matrix(p$descriptorfile.S , path=p$tmp.datadir ) 
-    Sloc = attach.big.matrix(p$descriptorfile.Sloc , path=p$tmp.datadir ) 
+    S = attach.big.matrix(p$descriptorfile.S , path=p$tmp.datadir )
+    Sloc = attach.big.matrix(p$descriptorfile.Sloc , path=p$tmp.datadir )
     p$spatial.domain="canada.east"  # force isobaths to work in levelplot
     datarange = log( c( 5, 800 ))
     dr = seq( datarange[1], datarange[2], length.out=150)
@@ -55,7 +54,7 @@ substrate.figures = function( DS=NULL, p=NULL ) {
         sp.lines( isobath.db( p=p, DS="isobath", depths=c( 200, 400 ), crs=p$internal.crs  ), col = "slateblue", cex=0.1 )
         sp.lines( coastline.db( p=p, crs=p$internal.crs), col = "steelblue", cex=0.1 )
       }
-    ) 
+    )
   }
 
 
