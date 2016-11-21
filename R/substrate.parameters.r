f7ce660c1b9b9f389f6440968ac9fc71a2ea55c3
Jae S Choi <choijaeseok@gmail.com>
2016-11-21 13:31:52 -0400

    force fields to estimate variogram

--- R/substrate.parameters.r
+++ R/substrate.parameters.r
@@ -26,18 +26,16 @@ substrate.parameters = function(DS="bio.substrate", p=NULL, resolution="canada.e
 
 
   if (DS=="bio.substrate.local") {
-    p$spacetime_variogram_engine = "gstat"  # "geoR" seg faults frequently ..
     p$spacetime_rsquared_threshold = 0.3 # lower threshold
     p$spacetime_distance_prediction = 5 # this is a half window km
     p$spacetime_distance_statsgrid = 5 # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
 
-    p$spacetime_variogram_engine = "gstat"  # "geoR" seg faults frequently ..
     p$sampling = c( 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2, 1.5, 1.75, 2 )  # fractions of median distance scale (dist.max, dist.min)/2 to try in local block search
 
     p$spacetime_engine = "gam" # see model form in spacetime.r (method="xyts")
-    p$spacetime_engine_modelformula = formula( 
+    p$spacetime_engine_modelformula = formula(
       substrate ~ s(plon,k=3, bs="ts") + s(plat, k=3, bs="ts") + s(plon, plat, k=40, bs="ts") +
-      s(z, k=3, bs="ts") + s(dZ, k=3, bs="ts" ) + s(ddZ, k=3, bs="ts" ) )  
+      s(z, k=3, bs="ts") + s(dZ, k=3, bs="ts" ) + s(ddZ, k=3, bs="ts" ) )
     # p$spacetime_engine_modelformula = formula( substrate ~ -1 + intercept
     #   + f( inla.group(log(z+1000) ), model="rw2")
     #   + f( inla.group(log(dZ+0.01)), model="rw2")
@@ -49,20 +47,20 @@ substrate.parameters = function(DS="bio.substrate", p=NULL, resolution="canada.e
     p$spacetime_covariate_modeltype="gam"
     p$spacetime_covariate_modelformula = p$spacetime_engine_modelformula
 
-    p$variables = list( Y="t", LOCS=c("plon", "plat"), COV=c("z", "dZ", "ddZ") ) 
+    p$variables = list( Y="t", LOCS=c("plon", "plat"), COV=c("z", "dZ", "ddZ") )
     p$spacetime_family = gaussian(link="log")
    ## data range is from -100 to 5467 m .. 1000 shifts all to positive valued by one -order of magnitude
     # or to make your own
     # p$spacetime_family = function(offset=0) {
     #   structure(list(
-    #     linkfun = function(mu) mu + offset, 
+    #     linkfun = function(mu) mu + offset,
     #     linkinv = function(eta) eta - offset,
-    #     mu.eta = function(eta) NA,  
-    #     valideta = function(eta) TRUE, 
+    #     mu.eta = function(eta) NA,
+    #     valideta = function(eta) TRUE,
     #     name = paste0("logexp(", offset, ")") ),
     #     class = "link-glm" )
     #
-    
+
     p$dist.max = 50 # length scale (km) of local analysis .. for acceptance into the local analysis/model
     p$dist.min = 2 # lower than this .. subsampling occurs
 
@@ -71,8 +69,7 @@ substrate.parameters = function(DS="bio.substrate", p=NULL, resolution="canada.e
 
     p$sbbox = spacetime_db( p=p, DS="statistics.box" ) # bounding box and resoltuoin of output statistics defaults to 1 km X 1 km
     p$non_convex_hull_alpha = 20  # radius in distance units (km) to use for determining boundaries
-    p$theta = 5 # FFT kernel bandwidth (SD of kernel) required for method "harmonic.1/kernel.density"
-    p$nsd = 6 # number of SD distances to pad boundaries with 0 for FFT  required in method  "harmonic.1/kernel.density
+    p$theta = p$pres # FFT kernel bandwidth (SD of kernel) required for method "harmonic.1/kernel.density"
 
     p$spacetime.noise = 0.001  # distance units for eps noise to permit mesh gen for boundaries
     p$quantile_bounds = c(0.001, 0.999) # remove these extremes in interpolations
