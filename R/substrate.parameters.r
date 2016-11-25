8e4164e447dd6ba6f84c5b6c785acffb9942022c
Jae S Choi <choijaeseok@gmail.com>
2016-11-25 00:14:56 -0400

    tweaks

--- R/substrate.parameters.r
+++ R/substrate.parameters.r
@@ -30,8 +30,8 @@ substrate.parameters = function(DS="bio.substrate", p=NULL, resolution="canada.e
     p$spacetime_distance_prediction = 5 # this is a half window km
     p$spacetime_distance_statsgrid = 5 # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
 
-    p$sampling = c( 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2, 1.5, 1.75, 2 )  # fractions of median distance scale (dist.max, dist.min)/2 to try in local block search
-
+    p$sampling = c( 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2, 1.5, 1.75, 2 )  
+    
     p$spacetime_engine = "gam" # see model form in spacetime.r (method="xyts")
     p$spacetime_engine_modelformula = formula(
       substrate ~ s(plon,k=3, bs="ts") + s(plat, k=3, bs="ts") + s(plon, plat, k=40, bs="ts") +
@@ -60,11 +60,7 @@ substrate.parameters = function(DS="bio.substrate", p=NULL, resolution="canada.e
     #     name = paste0("logexp(", offset, ")") ),
     #     class = "link-glm" )
     #
-
-    p$dist.max = 50 # length scale (km) of local analysis .. for acceptance into the local analysis/model
-    p$dist.min = 2 # lower than this .. subsampling occurs
-
-    p$n.min = 30 # n.min/n.max changes with resolution: at p$pres=0.25, p$dist.max=25: the max count expected is 40000
+    p$n.min = 30 # n.min/n.max changes with resolution
     p$n.max = 8000 # numerical time/memory constraint -- anything larger takes too much time
 
     p$sbbox = spacetime_db( p=p, DS="statistics.box" ) # bounding box and resoltuoin of output statistics defaults to 1 km X 1 km
